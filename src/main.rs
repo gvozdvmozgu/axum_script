4807b73b43d7a75d5b95860829755f9412480d33
Tom Nielsen <tanielsen@gmail.com>
2024-05-31 22:10:04 +0100

    attempts at route map

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
-use deno_core::error::AnyError;
+use deno_core::op2;
+use deno_core::{Extension, OpState};
+use std::collections::HashMap;
 use std::env;
 use std::rc::Rc;
+use std::sync::{Mutex, OnceLock};
+
+/*const ROUTES: OnceCell<HashMap<String, v8::Global<v8::Function>>> = OnceCell::new();
+
+fn routes_map() -> &'static Mutex<HashMap<String, v8::Global<v8::Function>>> {
+    static ARRAY: OnceLock<Mutex<Vec<u8>>> = OnceLock::new();
+    ARRAY.get_or_init(|| Mutex::new(vec![]))
+}
+
+#[op2()]
+fn op_route(#[string] path: &str, #[global] router: v8::Global<v8::Function>) {
+    let r = ROUTES;
+    let &mut current_routes = r.get_mut().unwrap();
+    current_routes.insert(String::from(path), router);
+    //routes.set(*current_routes);
+    dbg!(path);
+    ()
+} */
 
 fn get_init_dir() -> String {
     let args: Vec<String> = env::args().collect();
