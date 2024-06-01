5be6d2805549db9a945df410625121d06d6e7344
Tom Nielsen <tanielsen@gmail.com>
2024-06-01 08:18:08 +0100

    call op

--- testapp/setup.js
+++ testapp/setup.js
@@ -1,6 +1,5 @@
 //import { op_route } from "ext:core/ops";
 
 Deno.core.print("core"); //, Object.keys(Deno.core).join(""));
-Deno.core.op_route("foo", () => {});
-core.ops.op_route("foo", () => {});
-op_route("foo", () => {});
+Deno.core.print(JSON.stringify(Deno)); //, Object.keys(Deno.core).join(""));
+Deno.core.ops.op_route("foo", () => {});
