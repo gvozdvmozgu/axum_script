e2b18b125cb4719b0f73ad074c1559cc7f49a500
Tom Nielsen <tanielsen@gmail.com>
2024-06-06 20:28:40 +0100

    spawning js thread, channel

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::collections::HashMap;
 use std::env;
 use std::rc::Rc;
 use std::thread;
+use tokio::sync::mpsc;
 /*const ROUTES: OnceCell<HashMap<String, v8::Global<v8::Function>>> = OnceCell::new();
 
 fn routes_map() -> &'static Mutex<HashMap<String, v8::Global<v8::Function>>> {
@@ -47,10 +48,10 @@ struct AppState {
 
 #[derive(Clone)]
 struct RouteState {
-    x: u64,
+    tx: mpsc::Sender<u32>,
 }
 #[tokio::main]
-async fn js_thread() {
+async fn js_thread(mut rx: mpsc::Receiver<u32>) {
     let dir = get_init_dir();
     let setup_path = [dir, String::from("setup.js")].concat();
 
@@ -77,17 +78,21 @@ async fn js_thread() {
         runtime: Rc::new(RefCell::new(js_runtime)),
     };
     run_route(state, "foo").await;
+    while let Some(message) = rx.recv().await {
+        println!("GOT = {}", message);
+    }
 }
 
 #[tokio::main]
 async fn main() {
+    let (tx, mut rx) = mpsc::channel(32);
     thread::spawn(|| {
-        js_thread();
-    })
-    .join()
-    .expect("Thread panicked");
-
-    let rstate = RouteState { x: 1 };
+        js_thread(rx);
+    });
+    //.join()
+    //.expect("Thread panicked");
+    print!("Starting server");
+    let rstate = RouteState { tx: tx };
     let app = Router::new()
         .route("/", get(route_handler))
         .with_state(rstate);
@@ -99,8 +104,9 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn route_handler(State(_state): State<RouteState>) -> Response<Body> {
-    Html("").into_response()
+async fn route_handler(State(state): State<RouteState>) -> Response<Body> {
+    state.tx.send(3).await.unwrap();
+    Html("HELLO FROM SERVER").into_response()
 }
 
 async fn run_route(state: AppState, route_name: &str) -> Response<Body> {
